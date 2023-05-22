00180d8d89f5ceda97e42723f866ad9f37694304
educacionchcml <educacion.manzanadelasluces@gmail.com>
2023-05-22 15:54:28 -0300

    tp4

---
+++ src/tp4/ej2/Ej2.java
@@ -0,0 +1,49 @@
+package tp1.tp4.ej2;
+
+import java.util.Scanner;
+
+import tp1.tp4.tj3.LectorEnteros;
+import tp1.tp4.tj3.RangoDeEnteros;
+
+public class Ej2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		LectorEnteros lector = new LectorEnteros(input);
+		final RangoDeEnteros RANGO_NRO_DOCUMENTO = new RangoDeEnteros(1000000, 99999999);
+		final RangoDeEnteros RANGO_ANIO_NACIMIENTO = new RangoDeEnteros(1900, 2019);
+		Alumno alu = null;
+		int dni = 0, anioNacimiento = 0;
+		String nombreCompleto;
+
+		while (alu == null) {
+			// TODO Procesar la carga de la persona.
+		}
+		input.close();
+		System.out.println(alu.toString());
+
+		if (alu.vive()) {
+			System.out.println("La persona vive");
+		} else {
+			System.out.println("La persona no vive");
+		}
+
+	}
+}
+
+public abstract class Persona {
+	private String nombreCompleto;
+	private int anioNacimiento;
+	private int anioFallecimiento;
+	private int anioAcual;
+	public Persona(String nombreCompleto, int anioNacimiento) {
+		this.nombreCompleto = nombreCompleto;
+		this.anioNacimiento = anioNacimiento;
+	}
+	
+	public abstract boolean vive();
+}
+
+public class Alumno extends Persona {
+	
+}
